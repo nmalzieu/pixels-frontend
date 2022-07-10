6cf4c5071dfca1b29dd9e29a030a2de64a486a59
nmalzieu <noe.malzieu@gmail.com>
2022-07-10 14:38:13 +0200

    Add aspect link

--- components/star.tsx
+++ components/star.tsx
@@ -12,6 +12,34 @@ const Star = ({
 }) => {
   const basicRotate = -21.23;
   const realRotate = rotate + basicRotate;
+  let pxlsLink = (
+    <a
+      target="_blank"
+      href={`${process.env.NEXT_PUBLIC_ASPECT_LINK}/${process.env.NEXT_PUBLIC_PIXEL_ERC721_ADDRESS}/${pxls[0]}`}
+      rel="noreferrer"
+    >
+      PXL #{pxls[0]}
+    </a>
+  );
+  if (pxls.length > 1) {
+    pxlsLink = (
+      <span>
+        PXLS #
+        {pxls.map((pxl, index) => (
+          <span key={pxl}>
+            {index > 0 && ","}
+            <a
+              target="_blank"
+              href={`${process.env.NEXT_PUBLIC_ASPECT_LINK}/${process.env.NEXT_PUBLIC_PIXEL_ERC721_ADDRESS}/${pxl}`}
+              rel="noreferrer"
+            >
+              {pxl}
+            </a>
+          </span>
+        ))}
+      </span>
+    );
+  }
   return (
     <div
       className={styles.star}
@@ -22,7 +50,7 @@ const Star = ({
       <div className={styles.message}>
         YOU OWN
         <br />
-        <span className={styles.underline}>PXL #{pxls.join(",")}</span>
+        {pxlsLink}
       </div>
     </div>
   );

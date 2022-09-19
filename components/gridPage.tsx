9b9100abe58b942a8716f686d1e66058740df626
nmalzieu <noe.malzieu@gmail.com>
2022-09-19 15:32:59 +0200

    Make colors uppercase

--- components/gridPage.tsx
+++ components/gridPage.tsx
@@ -311,7 +311,7 @@ const GridPage = () => {
         temporaryColor.red,
         temporaryColor.green,
         temporaryColor.blue
-      ).toLowerCase();
+      ).toUpperCase();
       const hexColorIndex = allColors.indexOf(hexColor);
       if (hexColorIndex >= 0) {
         colorizations.push({
